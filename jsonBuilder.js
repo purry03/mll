06dcf4ea1c04bc8a91f84297b71fed90848e3f25
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-05-04 20:49:23 +0100

    Add files via upload

---
+++ jsonBuilder.js
@@ -0,0 +1,65 @@
+exports.buildJson = function (notificationType, issuerName, origin, destination, volume, status, validatedStatus, issuedDate, issuerId, discordRoleId){
+  let notificationMessage = '**' + issuerName + '** has put up a new rush shipping contract, see below for details.'
+  if (notificationType == 'reminderNotification') {
+    notificationMessage = "**REMINDER:** There was a rush contract put up over 12 hours ago and it's still outstanding"
+  }
+
+    let jsonData = {
+        "username": "MLL Rush Shipping Notification",
+        "avatar_url": "https://multi-lemm-logistics.com/img/logo.png",
+        "content": `<@&${discordRoleId}>`,
+        "embeds": [
+            {
+                "author": {
+                    "name": "MLL Rush Shipment Bot",
+                    "url": "http://multi-lemm-logistics.com",
+                    "icon_url": "https://multi-lemm-logistics.com/img/logo.png"
+                },
+                "title": "MLL Rush Shipment Notification",
+                "url": "http://multi-lemm-logistics.com/contracts",
+                "description": `${notificationMessage}`,
+                "color": 16757309,
+                "fields": [
+                    {
+                        "name": "Origin",
+                        "value": `${origin}`,
+                        "inline": true
+                    },
+                    {
+                        "name": "Destination",
+                        "value": `${destination}`,
+                        "inline": true
+                    },
+                    {
+                        "name": "Volume",
+                        "value": `${volume.toLocaleString("en-GB")} m3`,
+                        "inline": true
+                    },
+                    {
+                        "name": "Status",
+                        "value": `${status}`,
+                        "inline": true
+                    },
+                    {
+                        "name": "Validated Status",
+                        "value": `${validatedStatus}`,
+                        "inline": true
+                    },
+                    {
+                        "name": "Issued Date",
+                        "value": `${issuedDate.toLocaleString('en-GB', {timeZone: 'UTC'})}`,
+                        "inline": true
+                    }
+                ],
+                "footer": {
+                    "text": `${issuerName}`,
+                    "url": `https://evewho.com/character/${issuerId}`,
+                    "icon_url": `https://images.evetech.net/characters/${issuerId}/portrait`
+                }
+            }
+        ]
+    };
+
+
+    return jsonData;
+}
