289d3fca32413efa7967c8b15998ca6f522b7408
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-05-04 20:51:40 +0100

    Added discord checkbox to settings page

--- public/js/settings.js
+++ public/js/settings.js
@@ -15,3 +15,20 @@ $('.mailCheckbox').change(function () {
         }
     });
 });
+
+$('.discordCheckbox').change(function () {
+
+    $.ajax({
+        type: "GET",
+        url: "/settings/toggle/discord",
+        cache: false,
+        crossDomain: true,
+        dataType: 'json',
+        xhrFields: {
+            withCredentials: true
+        },
+        error: function (data) {
+            alert("Setting updated")
+        }
+    });
+});
