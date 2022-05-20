85bb1a833812df1e133260d2838dd4ad6169c536
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-05-20 17:01:28 +0100

    Moved JS about

--- public/js/jf-routes.js
+++ public/js/jf-routes.js
@@ -492,3 +492,17 @@ var forEach = function(object, block, context) {
 		resolve.forEach(object, block, context);
 	}
 };
+
+
+var $rows = $('#jf-routes tr');
+$('#search').keyup(function() {
+
+    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+        reg = RegExp(val, 'i'),
+        text;
+
+    $rows.show().filter(function() {
+        text = $(this).text().replace(/\s+/g, ' ');
+        return !reg.test(text);
+    }).hide();
+});
