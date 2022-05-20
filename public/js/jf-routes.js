34f5e61e2b24f3dacd611837a1511794c9efbbd4
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-05-20 17:07:33 +0100

    added slash to ISK M3 calc

--- public/js/jf-routes.js
+++ public/js/jf-routes.js
@@ -494,15 +494,28 @@ var forEach = function(object, block, context) {
 };
 
 
-var $rows = $('#jf-routes tr');
-$('#search').keyup(function() {
+function search_table(){
+  // Declare variables
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("jf-routes");
+  tr = table.getElementsByTagName("tr");
 
-    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
-        reg = RegExp(val, 'i'),
-        text;
-
-    $rows.show().filter(function() {
-        text = $(this).text().replace(/\s+/g, ' ');
-        return !reg.test(text);
-    }).hide();
-});
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td") ;
+    for(j=0 ; j<td.length ; j++)
+    {
+      let tdata = td[j] ;
+      if (tdata) {
+        if (tdata.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+          break ;
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+}
