2a03b1980d6f47be349982100f2e8d97ff076ce0
BradGlynne <104631391+BradGlynne@users.noreply.github.com>
2022-05-23 17:23:42 +0100

    rollback changes to jf-routes

--- index.js
+++ index.js
@@ -308,22 +308,15 @@ app.get("/perjumpcalculator", (req, res) => {
 
 
 app.get("/jf-routes", (req, res) => {
-  let standardRoutePromise = Routes.find({}).exec();
-  let intraRoutePromise = Routes.find({ {routeType: "Intra-Drones"} }).exec();
-  Promise.all ([standardRoutePromise, intraRoutePromise]).then((data) => {
-    res.render("jf-routes.ejs", { routes: data[0], intraRoutes: data[1] });
-  });
+    Routes.find({}, (err, routes) => {
+        if (err) {
+            res.sendStatus(500);
+        }
+        else {
+            res.render("jf-routes.ejs", { routes });
+        }
+    })
 });
-//    Routes.find({ {routeType: "Intra-Drones"} })
-//    Routes.find({}, (err, routes) => {
-//        if (err) {
-//            res.sendStatus(500);
-//        }
-//        else {
-//            res.render("jf-routes.ejs", { routes });
-//        }
-//    })
-//});
 
 app.get("/jfcalculator", (req, res) => {
     Routes.find({}, (err, routes) => {
